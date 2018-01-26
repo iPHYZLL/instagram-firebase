ea0c57d84e960e075d412ee055502621f26bc5fb
Alen <alen.kirm@gmail.com>
2018-01-27 00:21:00 +0100

    Episode 1: Added icons to registration text field

---
+++ Instagram-Firebase-Course/UITextField+Extension.swift
@@ -0,0 +1,9 @@
+//
+//  UITextField+Extension.swift
+//  Instagram-Firebase-Course
+//
+//  Created by Alen Kirm on 26. 01. 18.
+//  Copyright © 2018 Alen Kirm. All rights reserved.
+//
+
+import Foundation
