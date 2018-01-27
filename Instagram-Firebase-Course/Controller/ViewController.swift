f6c101fe79960c4259deff36f131f642d7e768a2
Alen <alen.kirm@gmail.com>
2018-01-27 15:59:50 +0100

    Added UIView anchor extensions for simplified constraint setting

--- Instagram-Firebase-Course/Controller/ViewController.swift
+++ Instagram-Firebase-Course/Controller/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
     let plusPhotoButton : UIButton = {
         let button = UIButton(type: .system)
         button.setImage(#imageLiteral(resourceName: "plus_photo"), for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
@@ -34,7 +33,6 @@ class ViewController: UIViewController {
         textField.borderStyle = .roundedRect
         textField.addLeftImageView(withImage: #imageLiteral(resourceName: "emailIcon"), andTintColor: UIColor.lightGray)
         textField.font = UIFont.systemFont(ofSize: 14)
-        textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
     
@@ -45,7 +43,6 @@ class ViewController: UIViewController {
         textField.borderStyle = .roundedRect
         textField.addLeftImageView(withImage: #imageLiteral(resourceName: "usernameIcon"), andTintColor: UIColor.lightGray)
         textField.font = UIFont.systemFont(ofSize: 14)
-        textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
     
@@ -57,7 +54,6 @@ class ViewController: UIViewController {
         textField.borderStyle = .roundedRect
         textField.addLeftImageView(withImage: #imageLiteral(resourceName: "passwordIcon"), andTintColor: UIColor.lightGray)
         textField.font = UIFont.systemFont(ofSize: 14)
-        textField.translatesAutoresizingMaskIntoConstraints = false
         return textField
     }()
     
@@ -65,10 +61,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         view.addSubview(plusPhotoButton)
-        plusPhotoButton.heightAnchor.constraint(equalToConstant: 140).isActive = true
-        plusPhotoButton.widthAnchor.constraint(equalToConstant: 140).isActive = true
+        
+        plusPhotoButton.anchor(top: view.topAnchor, paddingTop: 40, right: nil, paddingRight: 0, left: nil, paddingLeft: 0, bottom: nil, paddingBottom: 0, width: 140, height: 140)
+        
         plusPhotoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        plusPhotoButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 40).isActive = true
         
         setupInputFields()
     }
@@ -76,20 +72,13 @@ class ViewController: UIViewController {
     fileprivate func setupInputFields() {
         
         let stackView = UIStackView(arrangedSubviews: [usernameTextField, emailTextField, passwordTextField, signUpButton])
-        stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.distribution = .fillEqually
         stackView.axis = .vertical
         stackView.spacing = 10
         
         view.addSubview(stackView)
         
-        NSLayoutConstraint.activate([
-            stackView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 40),
-            stackView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -40),
-            stackView.heightAnchor.constraint(equalToConstant: 200)
-        ])
-        
-        stackView.anchor(top: plusPhotoButton.bottomAnchor, paddingTop: 20)
+        stackView.anchor(top: plusPhotoButton.bottomAnchor, paddingTop: 20, right: view.rightAnchor, paddingRight: 40, left: view.leftAnchor, paddingLeft: 40, bottom: nil, paddingBottom: 0, width: nil, height: 200)
         
     }
     
--- Instagram-Firebase-Course/Extensions/UIView+Extension.swift
+++ Instagram-Firebase-Course/Extensions/UIView+Extension.swift
@@ -10,8 +10,34 @@ import UIKit
 
 extension UIView {
     
-    func anchor(top : NSLayoutYAxisAnchor, paddingTop : CGFloat) {
-        self.topAnchor.constraint(equalTo: top, constant: paddingTop)
+    func anchor(top : NSLayoutYAxisAnchor?, paddingTop : CGFloat, right : NSLayoutXAxisAnchor?, paddingRight : CGFloat, left : NSLayoutXAxisAnchor?, paddingLeft : CGFloat, bottom :  NSLayoutYAxisAnchor?, paddingBottom : CGFloat, width : CGFloat?, height : CGFloat?) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: paddingBottom).isActive = true
+        }
+        
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+        
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
     }
     
 }
